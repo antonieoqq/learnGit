7f1571efe30f991a5321abf92313501b3262e5f1
antonieoqq <55801684.com>
2017-06-07 15:45:54 +0800

    delete files

--- Dragon/Assets/Scripts/Managers/BattleManager2D.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BattleManager2D : Singleton<BattleManager2D>
-{ 
-    public Vector2 PlayerCharactorPos { get; set; }
-}
